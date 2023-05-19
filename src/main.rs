9a847d139e383bf5d85403a9d2920ef5b6f81981
Albert Keith <albertkeith9312@hotmail.com>
2023-05-19 02:48:03 -0700

    First Deployment

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
+use std::net::TcpListener;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
+use secrecy::ExposeSecret;
+use sqlx::postgres::PgPoolOptions;
+
+    
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(),std::io::stdout);
+    init_subscriber(subscriber);
+
+    // Panic if we can't read configuration
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&configuration.database.connection_string().expose_secret())
+        .expect("Failed to create Postgres connection pool.");
+    
+
+    // We have removed the hard-coded `8000` - it's now coming from our settings!
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+        );
+    let listener = TcpListener::bind(address)?;
+    run(listener,connection_pool)?.await
 }
